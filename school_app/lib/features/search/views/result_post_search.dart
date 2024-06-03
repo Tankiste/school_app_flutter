45f89adb3a800d961ef0f0624b0b1a3cd7bf0c9a
Kengni-Joseph <joseph.kengni@2025.ucac-icam.com>
2024-06-03 06:40:25 +0200

    result post search

---
+++ school_app/lib/features/search/views/result_post_search.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:school_app/widgets/widgets.dart';
+
+class ResultPostSearch extends StatefulWidget {
+  const ResultPostSearch({super.key});
+
+  @override
+  State<ResultPostSearch> createState() => _ResultPostSearchState();
+}
+
+class _ResultPostSearchState extends State<ResultPostSearch> {
+  bool isLiked = false;
+
+  void likePost() {
+    setState(() {
+      isLiked = !isLiked;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var wd = MediaQuery.of(context).size.width;
+    var ht = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                    height: ht * 0.4,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20))),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20)),
+                      child: Image.asset(
+                        'assets/images/brainstorming.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+                Positioned(
+                    left: 15,
+                    top: ht * 0.065,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: SizedBox(
+                          height: ht * 0.045,
+                          width: wd * 0.09,
+                          child: ReturnButton()),
+                    ))
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: ht * 0.05,
+                            width: ht * 0.05,
+                            decoration: BoxDecoration(shape: BoxShape.circle),
+                            child: ClipOval(
+                              child: Image.asset("assets/images/supplier.png",
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: ht * 0.026,
+                                width: wd * 0.25,
+                                child: Text(
+                                  "Davis Bryan",
+                                  overflow: TextOverflow.ellipsis,
+                                  style: GoogleFonts.openSans(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              SizedBox(
+                                height: ht * 0.02,
+                                width: wd * 0.1,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'O3',
+                                      style: GoogleFonts.openSans(
+                                          fontSize: 13,
+                                          color: Colors.grey.shade500,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          GestureDetector(
+                            onTap: likePost,
+                            child: Icon(
+                              isLiked
+                                  ? Icons.favorite
+                                  : Icons.favorite_border_outlined,
+                              color: isLiked ? Colors.red : Colors.black,
+                              size: 28,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 2),
+                            child: Text(
+                              '120',
+                              style: GoogleFonts.openSans(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          SizedBox(
+                            width: wd * 0.03,
+                          ),
+                          Icon(
+                            FontAwesomeIcons.comment,
+                            size: 25,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 2),
+                            child: Text(
+                              '57',
+                              style: GoogleFonts.openSans(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          SizedBox(
+                            width: wd * 0.03,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 2),
+                            child: GestureDetector(
+                                onTap: () {},
+                                child: Icon(
+                                    FontAwesomeIcons.solidShareFromSquare,
+                                    size: 20)),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    '1 Week',
+                    style: GoogleFonts.openSans(
+                        fontSize: 15,
+                        color: Colors.grey.shade600,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text('View all comments',
+                        style: GoogleFonts.openSans(
+                          fontSize: 13,
+                          color: Colors.grey.shade400,
+                          fontWeight: FontWeight.w400,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Description',
+                    style: GoogleFonts.openSans(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                      'Wonderful experience with the upcoming technologies we tested',
+                      style: GoogleFonts.openSans(
+                        fontSize: 13,
+                        color: Colors.grey.shade600,
+                        fontWeight: FontWeight.w400,
+                      )),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
